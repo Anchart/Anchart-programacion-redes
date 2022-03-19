60dab5fb49e9b82c5d1892f1af89ef21ee375848
Anchart <83768957+Anchart@users.noreply.github.com>
2022-03-18 21:42:13 -0300

    Create celular.java

---
+++ celular.java
@@ -0,0 +1,21 @@
+//Creo la clase celular:
+package ejemplopkg;
+
+public class Celular {
+ 
+       //atributos de la clase
+	private String marca;
+	private String modelo;
+	private String color;
+
+public Celular(){
+}
+	// método hacer llamada
+		public void llamar(String nombre){
+			System.out.println("LLamando a "+nombre);
+		}	
+	
+	
+	
+
+ }
